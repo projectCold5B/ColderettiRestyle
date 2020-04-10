eb4afa512174146606a73e77b94b0c84bc638e0b
gabrielOrru <60508488+gabrielOrru@users.noreply.github.com>
2020-04-10 16:54:25 +0200

    Aggiornamento pagina nuovoOrdine: inserimenti php

--- coldiretti/nuovoOrdine.php
+++ coldiretti/nuovoOrdine.php
@@ -1,22 +1,17 @@
 <?php
-
-
-
 require 'php/db.php';
 $db = new database;
-
 require 'php/obj.php';
 $obj= new objectclass;
 $U=$db->CheckLog();
-if(!$U){
+if(!$U)
   echo "<script>alert('effettua il login');</script>".file_get_contents("index.php");
+?>
 
-}
- ?>
 <!DOCTYPE html>
 <html lang="it">
 <head>
-  <title>Nuovo Ordine</title>
+  <title>Nuovo ordine</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
@@ -29,10 +24,9 @@ if(!$U){
 <body>
 
 <?php
-
 $obj->Navbar($U);
-
 ?>
+
 <div class="container-fluid text-center">
   <div class="row content">
     <div class="col-sm-2 sidenav">
@@ -41,76 +35,58 @@ $obj->Navbar($U);
       <p><a href="#">Link</a></p>
     </div>
     <div class="col-sm-8 text-left">
-      <h1>Nuovo ordine</h1><br>
+      <h1>Nuovo ordine</h1>
+      <p>In questa pagina puoi effettuare un nuovo ordine</p>
 
-    <form action="php/ordine.php" method="post">
-      <table class="table table-bordered">
-        <thead>
-        <tr>
-          <th>Prodotto</th>
-          <th>Quantità</th>
-          <th>Descrizione</th>
-        </tr>
-        </thead>
-        <tr>
-          <td><input type="checkbox" name="carote"><label for="carote">Carote</label><br></td>
-          <td><input type="textbox" name="quantitàC" placeholder="Inserisci la quantità"></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td><input type="checkbox" name="finocchi"><label for="finocchi">Finocchi</label><br></td>
-          <td><input type="textbox" name="quantitàFi" placeholder="Inserisci la quantità"></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td><input type="checkbox" name="uova"><label for="uova">Uova</label><br></td>
-          <td><input type="textbox" name="quantitàU" placeholder="Inserisci la quantità"></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td><input type="checkbox" name="banane"><label for="banane">Banane</label><br></td>
-          <td><input type="textbox" name="quantitàB" placeholder="Inserisci la quantità"></td>
-          <td></td>
-        </tr>
-         <tr>
-          <td><input type="checkbox" name="mele"><label for="mele">Mele</label><br></td>
-          <td><input type="textbox" name="quantitàM" placeholder="Inserisci la quantità"></td>
-          <td></td>
-        </tr>
-         <tr>
-          <td><input type="checkbox" name="fragole"><label for="fragole">Fragole</label><br></td>
-          <td><input type="textbox" name="quantitàFr" placeholder="Inserisci la quantità"></td>
-          <td></td>
-        </tr>
-         <tr>
-          <td><input type="checkbox" name="iceberg"><label for="iceberg">Iceberg</label><br></td>
-          <td><input type="textbox" name="quantitàI" placeholder="Inserisci la quantità"></td>
-          <td></td>
-        </tr>
-         <tr>
-          <td><input type="checkbox" name="arance"><label for="arance">Arance</label><br></td>
-          <td><input type="textbox" name="quantitàA" placeholder="Inserisci la quantità"></td>
-          <td></td>
-        </tr>
-      </table>
-        <center><button type="button" class="btn btn-default">Effettua ordine</button></center><br>
-    </form>
-  </div>
+       <?php
 
-    <div class="col-sm-2 sidenav">
-      <div class="well">
-        <p>ADS</p>
-      </div>
-      <div class="well">
-        <p>ADS</p>
-      </div>
-    </div>
-  </div>
-</div>
+       //connessione al db
+       $connection=$db->Connect();
+       if (!$connection) die(mysqli_connect_error());
 
-<footer class="container-fluid text-center">
-  <p>Footer Text</p>
-</footer>
+       $query = "SELECT Nome, Descrizione FROM prodotti";
+       $result = mysqli_query($connection, $query);
+       if (@mysqli_num_rows($result)>=1) {
+         echo "
+              <form action='php/ordine.php' method='post'>
+               <table class='table table-bordered'>
+                 <thead>
+                   <tr>
+                     <th>Nome</th>
+                     <th>Descrizione</th>
+                     <th>Quantità</th>
+                  </tr>
+                </thead>";
+       while($row = $result->fetch_assoc()) {
+          echo "<tr><td>"."<input type='checkbox' name='".$row["Nome"]."'>" . $row["Nome"]. "</td><td>" . $row["Descrizione"] . "</td><td>" . "<input type='text' name='quantita' placeholder='Inserisci la quantità'>" . "</td></tr>";
+       }
+       echo "</table>
+             <center><button type='button' class='btn btn-default'>Effettua ordine</button></center><br>
+             </form></div>";
+       }
+       else
+       {
+         echo "</table><p>Non ci sono prodotti disponibili</form></div>";
+       }
 
-</body>
-</html>
+       //disconnessione dal db
+       $db->Disconnect($connection);
+       ?>
+
+       <div class="col-sm-2 sidenav">
+         <div class="well">
+           <p>ADS</p>
+         </div>
+         <div class="well">
+           <p>ADS</p>
+         </div>
+       </div>
+       </div>
+       </div>
+
+       <footer class="container-fluid text-center">
+       <p>Footer Text</p>
+       </footer>
+
+       </body>
+       </html>
