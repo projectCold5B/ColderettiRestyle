2851ea6d3433660e55a4c74e4a832bcf499859f3
nicolo <62823209+projectastz@users.noreply.github.com>
2020-04-14 11:18:24 +0200

    modifica carrello

--- coldiretti/Carrello.php
+++ coldiretti/Carrello.php
@@ -9,6 +9,10 @@ if(!$U)
 
 
 
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+
+
+
 
 
 if(isset($_POST['number_remove'])) {  
@@ -21,6 +25,23 @@ unset($_SESSION['cart'][($tina-1)]);
  echo "<script>alert('elemento rimosso');</script>";
     
 } 
+
+
+if(isset($_POST['number_modify'])&& isset($_POST['Modify'])) {  
+
+$i=$_POST['number_modify'];
+
+
+$_SESSION['cart'][$i][1]=$_POST['Modify'];
+
+ echo "<script>alert('elemento modificato');</script>";
+    
+} 
+
+}
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -61,7 +82,7 @@ $obj->Navbar($U);
        if ($max>=1) {
 
       for($i=0; $i<$max; $i++){ 
-             echo "<form method='post' id='".$_SESSION['cart'][$i][0]."' action='".htmlentities($_SERVER['PHP_SELF'])."' >
+             echo "
   <div class='card' style=' border :1px solid rgba(0,0,0,.125);'>
   <input type='hidden' name='Nome' value='".$_SESSION['cart'][$i][0]."'>
   <input type='hidden' name='number_remove' value='".$i."'>
@@ -69,12 +90,67 @@ $obj->Navbar($U);
 
   <h5 class='card-title'>".$_SESSION['cart'][$i][0]." - quantita : ".$_SESSION['cart'][$i][1]."</h5>
   <div class='card-body' style='display:flex;flex-direction:row-reverse'>
-    <input  class='btn' type='submit' value='Rimuovi'>
+
+    <button  type='button' class='btn btn-danger'  style='margin:10px;' data-toggle='modal' data-target='#RemoveModal'>Rimuovi</button>
+
+  <button type='button' class='btn btn-warning' style='margin:10px;' data-toggle='modal' data-target='#ModifyModal'>
+  Modifica
+</button>
 
 
 
   </div>
-  </div></form>";
+  </div>
+
+
+  </form>
+ <!-- Modal -->
+<div class='modal fade' id='RemoveModal' tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true' >
+  <div class='modal-dialog' role='document'>
+    <div class='modal-content' style='width:75%;'>
+      <div class='modal-header'>
+        <h5 class='modal-title' id='exampleModalLabel'>Rimuovi - ".$_SESSION['cart'][$i][0]."</h5>
+        <button type='button' class='close' data-dismiss='modal' aria-label='Close'>
+          <span aria-hidden='true'>&times;</span>
+        </button>
+      </div>
+      <div class='modal-body'><form method='post' id='".$_SESSION['cart'][$i][0]."' action='".htmlentities($_SERVER['PHP_SELF'])."' >
+        <input type='hidden' name='number_remove' value='".$i."'>
+          <h1 class='modal-title'>Vuoi Rimuovere l'oggetto?</h1>
+          </form>
+      </div>
+      <div class='modal-footer'>
+        <button type='button' class='btn btn-danger' data-dismiss='modal'>Close</button>
+        <button type='button' class='btn btn-success' onclick=document.getElementById('".$_SESSION['cart'][$i][0]."').submit();>Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+  <!-- Modal -->
+<div class='modal fade' id='ModifyModal' tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true' >
+  <div class='modal-dialog' role='document'>
+    <div class='modal-content' style='width:75%;'>
+      <div class='modal-header'>
+        <h5 class='modal-title' id='exampleModalLabel'>Modifica - ".$_SESSION['cart'][$i][0]."</h5>
+        <button type='button' class='close' data-dismiss='modal' aria-label='Close'>
+          <span aria-hidden='true'>&times;</span>
+        </button>
+      </div>
+      <div class='modal-body'>
+        <form method='post' id='Modify".$_SESSION['cart'][$i][0]."' action='".htmlentities($_SERVER['PHP_SELF'])."' >
+          <input type='hidden' name='number_modify' value='".$i."'>
+    
+          <input type='number' id='Modify' name='Modify' value='".$_SESSION['cart'][$i][1]."' step='1'>
+          </form>
+      </div>
+      <div class='modal-footer'>
+        <button type='button' class='btn btn-danger' data-dismiss='modal'>Close</button>
+        <button type='button' class='btn btn-success' onclick=document.getElementById('Modify".$_SESSION['cart'][$i][0]."').submit();>Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>";
         }
     
        }
